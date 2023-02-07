da17371803bb0c7e55a7cf91b79ab33083980ab8
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-02-07 23:46:29 +0100

    uvs

--- Assets/Scripts/SphericalMesh.cs
+++ Assets/Scripts/SphericalMesh.cs
@@ -67,7 +67,6 @@ public class SphericalMesh : MonoBehaviour {
         int y = Mathf.FloorToInt(normY * heightMap.height);
     
         float height = heightMap.GetPixel(x, y).grayscale;
-        //Debug.Log($"{coordinate.longitude} {coordinate.latitude} {height}");
         
         return height;
     }
@@ -106,11 +105,12 @@ public class SphericalMesh : MonoBehaviour {
             mesh.RecalculateBounds();
             mesh.RecalculateTangents();
             mesh.RecalculateUVDistributionMetrics();
+            mesh.uv = _meshData[i].uvs;
             // uv's
         }
     }
 
-    private MeshData[] GenerateFaces(int resolution) {
+    private static MeshData[] GenerateFaces(int resolution) {
         MeshData[] allMeshData = new MeshData[6];
         Vector3[] faceNormals = {
             Vector3.up, 
@@ -129,10 +129,11 @@ public class SphericalMesh : MonoBehaviour {
         return allMeshData;
     }
 
-    private MeshData CreateFace(Vector3 normal, int resolution) {
+    private static MeshData CreateFace(Vector3 normal, int resolution) {
         Vector3 axisA = new Vector3(normal.y, normal.z, normal.x);
         Vector3 axisB  = Vector3.Cross(normal, axisA);
         Vector3[] vertices = new Vector3[resolution * resolution];
+        Vector2[] uvs = new Vector2[resolution * resolution];
         int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
         int triIndex = 0;
 
@@ -143,6 +144,7 @@ public class SphericalMesh : MonoBehaviour {
                 Vector2 t = new Vector2(x, y) / (resolution - 1f);
                 Vector3 point = normal + axisA * (2 * t.x - 1) + axisB * (2 * t.y - 1);
                 vertices[vertexIndex] = point;
+                uvs[vertexIndex] = new Vector2((float)x / resolution, (float)y / resolution);
 
                 if (x != resolution - 1 && y != resolution - 1) {
                     triangles[triIndex + 0] = vertexIndex;
@@ -156,7 +158,7 @@ public class SphericalMesh : MonoBehaviour {
             }
         }
 
-        return new MeshData(vertices, triangles);
+        return new MeshData(vertices, triangles, uvs);
     }
 
     //public static Vector3 PointOnCubeToPointOnSphere(Vector3 p) => Vector3.Normalize(p);
@@ -183,11 +185,13 @@ public struct Coordinate {
 }
 
 public struct MeshData {
-    public MeshData(Vector3[] vertices, int[] triangles) {
+    public MeshData(Vector3[] vertices, int[] triangles, Vector2[] uvs) {
         this.vertices = vertices;
         this.triangles = triangles;
+        this.uvs = uvs;
     }
 
     public Vector3[] vertices;
     public int[] triangles;
+    public Vector2[] uvs;
 } 
