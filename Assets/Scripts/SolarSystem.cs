1e596fb63e2d716c656e68974f7cc60a7a7d64c4
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-02-11 16:26:03 +0100

    Camera Zoom, Ocean Mask

---
+++ Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour {
+    private static readonly int Rotation = Shader.PropertyToID("_Rotation");
+    
+    [SerializeField] private Transform sun;
+    [SerializeField] private float rotation;
+    [SerializeField] private Skybox skybox;
+    private Material _material;
+    
+    private void Awake() {
+        _material = Instantiate(skybox.material);
+        skybox.material = _material;
+    }
+
+    private void Update() {
+        rotation = Time.time * .1f % 360f;
+        skybox.material.SetFloat(Rotation, rotation);
+    }
+
+    private void OnDestroy() {
+        if (_material != null) 
+            Destroy(_material);
+    }
+}
