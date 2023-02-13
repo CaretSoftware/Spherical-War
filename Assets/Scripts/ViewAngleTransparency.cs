3dbf873e4217cf394d4734141454ef725c48ebcc
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-02-13 06:38:25 +0100

    ViewAngleTransparency Earths Atmosphere Shadow

--- Assets/Scripts/ViewAngleTransparency.cs
+++ Assets/Scripts/ViewAngleTransparency.cs
@@ -1,27 +1,29 @@
 ﻿using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class ViewAngleTransparency : MonoBehaviour {
-    private static readonly int Color1 = Shader.PropertyToID("_Color");
+    private static readonly int Color1 = Shader.PropertyToID("_BaseColor");
     
     [SerializeField] private Transform cam;
-    [SerializeField] private Transform atmosphereShadow;
-    [FormerlySerializedAs("_alphaFull")] [SerializeField] private float alphaFull;
+    [SerializeField] private Transform atmosphereShadowTransform;
+    [SerializeField] private MeshRenderer atmosphereShadow;
     
-    private Color color = Color.black;
+    public Color color = Color.black;
     private Material _material;
+    private float alphaMax;
 
     private void Awake() {
-        _material = Instantiate(atmosphereShadow.GetComponent<MeshRenderer>().material);
-        
+        _material = Instantiate(atmosphereShadow.material);
+        atmosphereShadow.material = _material;
+        color = _material.GetColor(Color1);
+        alphaMax = color.a;
     }
 
     private void Update() {
-        Vector3 direction = (cam.position - atmosphereShadow.position).normalized;
-        float dot = Vector3.Dot(direction, atmosphereShadow.forward);
+        Vector3 direction = (cam.position - atmosphereShadowTransform.position).normalized;
+        float dot = Vector3.Dot(direction, atmosphereShadowTransform.forward);
         float t = Mathf.InverseLerp(-1f, 1f, dot);
-        float alpha = Mathf.Lerp(0f, alphaFull, t);
+        float alpha = Mathf.Lerp(0f, alphaMax, t);
         color.a = alpha;
         _material.SetColor(Color1, color);
     }
